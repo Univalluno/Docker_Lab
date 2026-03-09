f8f7c5abd55c572702897e5113cc8e4673f71a6a
Univalluno <146486793+Univalluno@users.noreply.github.com>
2026-03-09 14:03:36 -0500

    Add Jerson service

---
+++ students/jerson/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const student = process.env.STUDENT_NAME || "Jerson";
+const hood = process.env.BARRIO || "San Antonio";
+
+app.get('/', (req, res) => {
+  const msg = `Hola, yo soy ${student} y vivo en ${hood}`;
+  fs.appendFileSync('/var/log/app/visitas.log', msg + '\n');
+  res.type('text/plain').send(msg);
+});
+
+app.get('/health', (req, res) => res.json({ ok: true }));
+
+app.listen(8080, () => console.log("Server running on 8080"));
